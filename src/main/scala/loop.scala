90e997707380e9b9ff6788e7fe3715c4e39e986d
Amit <baghelaa@gmail.com>
2024-01-28 22:10:53 +0530

    world count

---
+++ src/main/scala/loop.scala
@@ -0,0 +1,24 @@
+object loop {
+
+  def main(args: Array[String]): Unit = {
+
+    var num = 1;
+    for (i <- 1 to 5) {
+      for (j <- 1 to i) {
+        print((num) + " ");
+
+      }
+      num = num + 1
+      println(" ")
+    }
+
+
+        for (i <- 1 to 5) {
+          for (j <- 1 to i) {
+            print((j) + " ");
+
+          }
+          println(" ")
+        }
+      }
+  }
